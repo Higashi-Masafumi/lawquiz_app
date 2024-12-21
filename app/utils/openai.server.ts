b0d4a6e5ee48469336fd4952631e0dab013aa13c
Masafumi Higashi <higashi-masafumi507@g.ecc.u-tokyo.ac.jp>
2024-12-22 02:39:03 +0900

    openaiの環境変数の読み込み修正

--- app/utils/openai.server.ts
+++ app/utils/openai.server.ts
@@ -6,7 +6,7 @@ import { fetchPostContent } from "./cms.server";
 import { registerGrade } from "./cms.server";
 import dotenv from "dotenv";
 dotenv.config();
-const openai = new OpenAI({ apiKey: process.env.VITE_OPENAI_API_KEY });
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 // 一つの採点項目の採点結果のスキーマ
 const scoringCriterionSchema = z.object({
