028fa14a6f0713caed2ebb3952c29b7e9268b7b5
Masafumi Higashi <higashi-masafumi507@g.ecc.u-tokyo.ac.jp>
2025-02-24 15:16:17 +0900

    不要なクラスを削除

--- app/routes/articles.$slug/accordion-card.tsx
+++ app/routes/articles.$slug/accordion-card.tsx
@@ -10,21 +10,13 @@ interface AccordionCardProps {
   title: string;
   trigger_color?: string;
   html_content: string;
-  font_size?: "sm" | "md" | "lg";
 }
 
 export function AccordionCard({
   title,
   trigger_color = "bg-gray-200",
   html_content,
-  font_size = "md",
 }: AccordionCardProps) {
-  const fontSizeClass = {
-    sm: "text-sm",
-    md: "text-base",
-    lg: "text-lg",
-  };
-
   return (
     <Accordion type="multiple">
       <AccordionItem value={title}>
